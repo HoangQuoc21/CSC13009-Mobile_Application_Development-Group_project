b4d55fcc174c76e91702556a75fd380d0c46309e
21120537 <145359313+THAQuanttt@users.noreply.github.com>
2023-11-16 17:35:25 +0700

    Add favorite Album

---
+++ app/src/main/java/com/example/album/Current_Album.java
@@ -0,0 +1,14 @@
+package com.example.album;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Current_Album extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_album);
+    }
+}
